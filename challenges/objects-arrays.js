5406a78065a35345cf41bbb81d3b7d19060fe815
vladmog <vladmog@gmail.com>
2019-05-10 09:29:44 -0700

    finished objects and array methods

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -68,7 +68,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university);
+}
+console.log(universities.sort())
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -77,12 +81,21 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i = 0; i < graduates.length; i++){
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`)
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i = 0; i < graduates.length; i++){
+  if (graduates[i].university.includes("Uni")){
+    uni.push(graduates[i].university)
+  }
+}
 console.log(uni);
+console.log(uni.length)
 
 
 // ==== ADVANCED Array Methods ====
@@ -107,6 +120,9 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach(function(element){
+  animalNames.push(`Name: ${element.animal_name}, Scientific: ${element.scientific_name}`)
+})
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -115,8 +131,17 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = []; //if this weren't const I'd = it to next line
+zooAnimals.map(function(element){
+  lowerCase.push(element.animal_name.toLowerCase()) //with a return statement here
+})
 console.log(lowerCase); 
+
+                      // let lowerCase = [];
+                      // lowerCase = zooAnimals.map(function(element){
+                      //   return element.animal_name.toLowerCase()
+                      // })
+                      // console.log(lowerCase); 
 
 /* Request 3: .filter() 
 
@@ -124,20 +149,35 @@ The zoos are concenred about animals with a lower population count. Find out whi
 
 */
 const largerPopulation = [];
-console.log(largerPopulation);
+zooAnimals.filter(function(element){
+  if(element.population < 5){
+      largerPopulation.push(element)
+  }
+})//I made it work but I'm pretty sure this isn't what you meant... 
+console.log(largerPopulation); //can't assign a new array to a const array, can only push to it
+
+                      // let largerPopulation = [];
+                      // largerPopulation = zooAnimals.filter(function(element){
+                      //   return element.population < 5;
+                      // })
+                      // console.log(largerPopulation);
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+let populationTotal = 0;
+populationTotal = zooAnimals.reduce(function(accumulator, element){
+  return accumulator + element.population;
+}, 0)
 console.log(populationTotal);
-
 
 /* 
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
+
+
 
