2e81a0d5d05e6b7f4bcf8d6e1c6c2d149c73fd69
vladmog <vladmog@gmail.com>
2019-05-10 08:27:36 -0700

    finished objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,46 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
-// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
-// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
-// Using your dinosaur objects, log answers to these questions:
-
-// How much did tyrannosaurus weigh?
-console.log();
-
-// What was the diet of a velociraptor?
-console.log();
-
-// How long was a stegosaurus?
-console.log();
-
-// What time period did tyrannosaurus live in?
-console.log();
-
-
-// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+const tyrannosaurus = {
+  "diet": "carnivorous",
+  "weight" : "7000kg",
+  "length": "12m",
+  "period": "Late Cretaceous",
+  roar: function(){
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+  }
+  // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+  const stegosaurus = {
+    "diet": "herbivorous",
+    "weight" : "2000kg",
+    "length": "9m",
+    "period": "Late Jurassic",
+    }
+  // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+  const velociraptor = {
+    "diet": "carnivorous",
+    "weight" : "15kg",
+    "length": "1.8m",
+    "period": "Late Cretaceous",
+    }
+  // Using your dinosaur objects, log answers to these questions:
+  
+  // How much did tyrannosaurus weigh?
+  console.log(tyrannosaurus.weight);
+  
+  // What was the diet of a velociraptor?
+  console.log(velociraptor.diet);
+  
+  // How long was a stegosaurus?
+  console.log(stegosaurus.length);
+  
+  // What time period did tyrannosaurus live in?
+  console.log(tyrannosaurus.period);
+  
+  
+  // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+  console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
